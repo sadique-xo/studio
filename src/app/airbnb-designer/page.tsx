2a37e8c74b70c92ab249398436684f73cab8ecfe
Sadique <sadique.xo@gmail.com>
2025-10-15 21:47:20 +0000

    now as we have landing page. make a new page /airbnb-designer and replic

---
+++ src/app/airbnb-designer/page.tsx
@@ -0,0 +1,19 @@
+import HeroSection from '@/components/sections/HeroSection';
+import AboutSection from '@/components/sections/AboutSection';
+import ServicesSection from '@/components/sections/ServicesSection';
+import PortfolioSection from '@/components/sections/PortfolioSection';
+import TestimonialsSection from '@/components/sections/TestimonialsSection';
+import CtaSection from '@/components/sections/CtaSection';
+
+export default function AirbnbDesignerPage() {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <PortfolioSection />
+      <TestimonialsSection />
+      <CtaSection />
+    </>
+  );
+}
