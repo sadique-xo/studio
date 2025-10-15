ac41389b171b1be942eeaffe0ed68b5dafc5a110
Sadique <sadique.xo@gmail.com>
2025-10-15 21:32:33 +0000

    I think you are making it more problematic and issue only.
    now it's tot

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className={cn("flex items-center justify-center cursor-pointer")} aria-label={`${siteSettings.title} logo`}>
-      <Image src="/icon.png" alt={`${siteSettings.title} logo`} width={120} height={40} />
+      <Image src="/Profile.PNG" alt={`${siteSettings.title} logo`} width={120} height={40} />
     </div>
   );
 }
